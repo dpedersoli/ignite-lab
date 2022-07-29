dea4576b6a1633e69c230a4a0c60993964395984
dpedersoli <dpmsengineer@gmail.com>
2022-07-29 15:33:26 -0300

    CHORE: authentication 70%

--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -1,12 +1,34 @@
+import { useState, useEffect } from 'react' //'useEffect' é executado quando a interface é renderizada
+
 import { NavigationContainer } from '@react-navigation/native'
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth' //'FirebaseAuthTypes' já tem a tipagem, então não preciso criar uma nova 
+
+import { Loading } from '../components/Loading'
 
 import { SignIn } from '../screens/SignIn'
 import { AppRoutes } from './app.routes'
 
 export const Routes = () => {
+  const [loading, setIsLoading] = useState(true) //estado de carregamentoa -> deve começar carregando (true)
+  const [user, setUser] = useState<FirebaseAuthTypes.User>() //aqui determino a tipagem do dado como a determina em 'FirebaseAuthTypes' dentro de '.User' -> se o usuário estiver logado terá algo aqui, se não, não terá nada
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(response => {
+      setUser(response);
+      setIsLoading(false)
+    }) // 'auth().onAuthStateChanged(res)' vai ficar observando se o usuário está autenticado ou não -> e se tiver ele vai armazenar ela dentro do 'setUser'
+
+    return subscriber; //assim que o 'useEffect' processar toda a lógica ele aplica a "limpeza" da memória
+  }, []) //dentro do array eu deixo o que estará sendo observado pelo 'useEffect' -> e toda vez que o conteúdo sinalizado dentro da array for alterado o 'useEffect' será acionado/executado
+
+  if (loading) { //se ainda não estiver autenticado ele permanece com o component 'Loading' na tela
+    return <Loading />
+  }
+
   return (
     <NavigationContainer>
-      <AppRoutes />
+      {user ? <AppRoutes /> : <SignIn />}
+      {/*Se o usuário exixtir (true) ele irá carregar o 'AppRoutes', se não, permanece na tela de login 'SignIn'*/}
     </NavigationContainer>
   )
 }
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -11,6 +11,7 @@ import { Input } from '../components/Input'
 import { Button } from '../components/Button'
 
 export const SignIn = () => {
+  const [isLoading, setIsLoading] = useState(false) //loading
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -20,6 +21,29 @@ export const SignIn = () => {
     if (!email || !password) { //se e-mail OU senha é nulo irá emitir um alerta
       return Alert.alert('Entrar', 'Informe e-mail e senha');
     }
+    setIsLoading(true);
+
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => {
+        console.error(error)
+        setIsLoading(false);
+
+        if (error.code == 'auth/invalid-email') { //a rota de 'auth/invalid-email' é uma das rotas de retorno de erro do firebase dentro do '.code', que no caso é do e-mail invávalido (existem muitas outras, e o ideal é setar todas as possibilidades dentro do 'error.code')
+          return Alert.alert('Entrar', 'Email inválido.') //a primeira aspas 'Entrar' seria o título do Alert do react-native, e a segunda aspas é o texto dentro do alert
+        }
+
+        if (error.code == 'auth/wrong-password') {
+          return Alert.alert('Entrar', 'Email ou senha inválido.')
+        }
+
+        if (error.code == 'auth/user-not-found') {
+          return Alert.alert('Entrar', 'Email ou senha inválido.')
+        }
+
+        return Alert.alert('Entrar', 'Não foi possível acessar')
+      }) //'auth()' é a promise de autenticação do firebase
+
   }
 
   return (
@@ -48,7 +72,8 @@ export const SignIn = () => {
         w="full"
         mt={4}
         onPress={handleSignIn}
-      />
+        isLoading={isLoading}
+      />{/*o próprio componente do native-base tem uma propriedade (isLoading) para efeito de 'laoding' -> ele desabilita a possibilidade de clicar no botão novamente e gera o 'load'*/}
 
     </VStack>
   )
