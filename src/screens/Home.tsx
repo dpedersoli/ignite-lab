b512296702962dd1861f522acf554b4f33bef88f
dpedersoli <dpmsengineer@gmail.com>
2022-08-08 22:02:39 -0300

    semtempoirmao

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,13 +1,13 @@
 import { useState, useEffect } from 'react'; //usando o 'useEffect' p/ renderizar a página a cada nova 'solicitação'
 import { Alert } from 'react-native'
 import auth from '@react-native-firebase/auth'
-import firestore from '@react-native-firebase/firestore' //importação do firestore p/ buscar dele os dados
+import firestore from '@react-native-firebase/firestore'; //importação do firestore p/ buscar dele os dados
 import { useNavigation } from '@react-navigation/native'
 import { HStack, IconButton, VStack, useTheme, Text, Heading, FlatList, Center } from 'native-base';
 import { SignOut } from 'phosphor-react-native'
 import { ChatTeardropText } from 'phosphor-react-native'
 
-import { dateFormat } from '../utils/firestoreDateFormate'
+import { dateFormat } from '../utils/firestoreDateFormat'
 
 import Logo from '../assets/logo_primary.svg'
 
@@ -46,10 +46,10 @@ export const Home = () => {
 
     const subscriber = firestore() //requisito que "vai no firestore, dentro da coleção 'orders' e faça um filtro como o abaixo ('.where')"
       .collection('orders')
-      .where('status', '==', statusSelected) //faça o filtro e peque as cosias onde o 'status' seja '==' ao valor de 'statusSelected' (que é 'open'/'closed')
+      .where('status', '==', statusSelected) //faça o filtro e peque as coisas onde o 'status' seja '==' ao valor de 'statusSelected' (que é 'open'/'closed')
       .onSnapshot(snapshot => {
         const data = snapshot.docs.map(doc => {
-          const { patrimony, description, status, created_at } = doc.data()
+          const { patrimony, description, status, created_at } = doc.data();
 
           return {
             id: doc.id,
@@ -58,14 +58,14 @@ export const Home = () => {
             status,
             when: dateFormat(created_at)
           } //vou retornar para o 'data' o conjunto de dados que quero utilizar
-        })//'onSnapshot' atualiza os dados em tempo real dentro da aplicação sem o usuário precisar fazer nada -> no caso eu acesso todos os 'docs' (documentos) dentro do 'snapshot' e passo um 'map' neles (array), e para cada item percorrido dentro do 'data' (no caso 'doc.data' pelo parâmetro passado) eu farei isso sobre as infos dentro de 'patrimony, description, status, created_at'
+        }); //'onSnapshot' atualiza os dados em tempo real dentro da aplicação sem o usuário precisar fazer nada -> no caso eu acesso todos os 'docs' (documentos) dentro do 'snapshot' e passo um 'map' neles (array), e para cada item percorrido dentro do 'data' (no caso 'doc.data' pelo parâmetro passado) eu farei isso sobre as infos dentro de 'patrimony, description, status, created_at'
 
-        setOrders(data)
-        setIsLoading(false)
-      }) // passo o valor da var 'data' p/ dentro do 'setOrders' e determino o 'loading' como 'false'
+        setOrders(data);
+        setIsLoading(false);
+      }); // passo o valor da var 'data' p/ dentro do 'setOrders' e determino o 'loading' como 'false'
 
-    return subscriber
-  }, [])
+    return subscriber;
+  }, [statusSelected]);
 
   return (
     <VStack flex={1} pb={6} bg="gray.700">
@@ -126,7 +126,7 @@ export const Home = () => {
               ListEmptyComponent={() => (
                 <Center>
                   <ChatTeardropText color={colors.gray[300]} size={40} />
-                  <Text color="gray.300" fontSize="xl" mt={6} textAlign="center" >
+                  <Text color="gray.300" fontSize="xl" mt={6} textAlign="center">
                     Você ainda não possui {'\n'}
                     solicitações {statusSelected === 'open' ? 'em andamento' : 'finalizadas'}
                   </Text>
--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,48 +1,49 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import { Alert } from 'react-native';
 import { VStack } from 'native-base';
-import { useNavigation } from '@react-navigation/native'
-import firestore from '@react-native-firebase/firestore'
+import { useNavigation } from '@react-navigation/native';
+import firestore from '@react-native-firebase/firestore';
 
-import { Header } from '../components/Header'
-import { Input } from '../components/Input'
-import { Button } from '../components/Button'
+import { Header } from '../components/Header';
+import { Input } from '../components/Input';
+import { Button } from '../components/Button';
 
-export const Register = () => {
-  const [isLoading, setIsLoading] = useState(false) //efeito de loading
-  const [patrimony, setPatrimony] = useState('') //id do equipamento
-  const [description, setDescription] = useState('') //descrição do problema no chamado
-  const navigation = useNavigation()
+export function Register() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [patrimony, setPatrimony] = useState('');
+  const [description, setDescription] = useState('');
 
-  const handleNewOrderRegister = () => {
-    if (!patrimony || !description) { //se 'patrimony' OU 'desciption' estiverem 'null' (sem valor) então retorna o Alert abaixo
-      return Alert.alert('Registrar', 'Preencha todos os campos')
+  const navigation = useNavigation();
+
+  function handleNewOrderRegister() {
+    if (!patrimony || !description) {
+      return Alert.alert('Registrar', 'Preencha todos os campos.');
     }
 
-    setIsLoading(true)
+    setIsLoading(true);
 
     firestore()
-      .collection('orders') //crio a coleção e dou nome de 'orders' -> se a coleção existir anteriormente dentro do meu firebase-firestore ele cria essa coleção automaticamente
+      .collection('orders')
       .add({
         patrimony,
         description,
         status: 'open',
-        created_at: firestore.FieldValue.serverTimestamp
-      }) //aqui eu passo o conteúdo do que eu quero adicionar -> no status eu passo se o 'chamado' está 'open' ou 'closed', e por padrão eu passei ele como 'open'; No 'created_at: firestore.FieldValue.serverTimestamp' eu uso o 'firebase' p/ falar a hora atual do chamado (função nativa do 'firestore')
+        created_at: firestore.FieldValue.serverTimestamp()
+      })
       .then(() => {
-        Alert.alert('Solicitação', 'Solicitação registrada com sucesso.')
-        navigation.goBack()
+        Alert.alert("Solicitação", "Solicitação registrada com sucesso.");
+        navigation.goBack();
       })
-      .catch((err) => {
-        console.error(err)
-        setIsLoading(false)
-        return Alert.alert('Solicitação', 'Não foi possível registrar o pedido.')
-      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+        return Alert.alert('Solicitação', 'Não foi possível registrar o pedido');
+      });
   }
 
   return (
     <VStack flex={1} p={6} bg="gray.600">
-      <Header title="Nova Solicitação" />
+      <Header title="Solicitação" />
 
       <Input
         placeholder="Número do patrimônio"
